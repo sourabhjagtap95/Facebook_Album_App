372b5b3af70bd392ea02a80ecdaa09edef4442cd
Sourabh Jagtap <sourabhjagtap95@gmail.com>
2016-08-31 20:47:23 +0530

    Update fbconfig.php

--- fbconfig.php
+++ fbconfig.php
@@ -13,9 +13,9 @@
     /*Facebook\Facebook is a service class provides an easy interface for working with all the components of the SDK.
     It passes an array of configuration options to the constructor.*/
     $fb = new Facebook\Facebook([
-       /* 'app_id' => 'appid',         //Your App ID
-        'app_secret' => 'appsecret',     //Your App Secret
-        'default_graph_version' => 'v2.5',*/
+        'app_id' => '1733591630236331',         //Your App ID
+        'app_secret' => 'bf0790db124ab31c80d959a852dfe536',     //Your App Secret
+        'default_graph_version' => 'v2.5',
     ]);
 $app_id = '1733591630236331';
 $app_secret = 'bf0790db124ab31c80d959a852dfe536';
