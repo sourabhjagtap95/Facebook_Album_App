ab3ef2acdb38ba443a2fbc5fcdc6dde58c5e1a29
Sourabh Jagtap <sourabhjagtap95@gmail.com>
2016-08-31 19:00:15 +0530

    Updated

--- fbconfig.php
+++ fbconfig.php
@@ -11,12 +11,12 @@
     /*Facebook\Facebook is a service class provides an easy interface for working with all the components of the SDK.
     It passes an array of configuration options to the constructor.*/
     $fb = new Facebook\Facebook([
-        'app_id' => '{AppID}',
-        'app_secret' => '{AppSecret}',
+        'app_id' => '1733591630236331',         //Your App ID
+        'app_secret' => 'bf0790db124ab31c80d959a852dfe536',     //Your App Secret
         'default_graph_version' => 'v2.5',
     ]);
-$app_id = 'AppId';
-$app_secret = 'AppSecret';
+$app_id = '1733591630236331';
+$app_secret = 'bf0790db124ab31c80d959a852dfe536';
 $fb_login_url = 'http://localhost/Facebook_app/index.php';
 session_start();
 ?>
