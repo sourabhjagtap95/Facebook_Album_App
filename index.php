74a3f98602082c67fdd549cde362368972719df4
Sourabh Jagtap <sourabhjagtap95@gmail.com>
2016-09-17 17:52:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,11 +15,11 @@ require_once __DIR__ . '/vendor/autoload.php';
     <title>Facebook Albums</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="2uAX5gLSPgsR0L1iM37-eZGUdiH0-rZQtz2-HHlQuoo" />
     <link rel="stylesheet" type="text/css" href="libs/resources/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="https://blueimp.github.io/Gallery/css/blueimp-gallery.min.css"/>
     <link rel="stylesheet" href="libs/resources/gallery_css/bootstrap-image-gallery.min.css"/>
     <script src="//fast.eager.io/cVEwvLt7my.js"></script>
-    <meta name="google-site-verification" content="2uAX5gLSPgsR0L1iM37-eZGUdiH0-rZQtz2-HHlQuoo" />
     <style type="text/css">
         @import url(https://fonts.googleapis.com/css?family=Nunito:300);
 
